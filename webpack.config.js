29ae9af0f198d4a248b28a9d0f5146973220824a
Marcio Silva <marcioadsworks@gmail.com>
2020-02-03 20:56:03 -0300

    bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './ex/index.js',
     output: {
         path: __dirname + '/public',
-        fileName: './bundle.js'
+        filename: './bundle.js'
     },
     devServer: {
         port: 8080,
